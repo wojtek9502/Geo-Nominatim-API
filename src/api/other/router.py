794d38d13bae195789d7d7a20d3e5ee2d3281699
wojtek9502 <wojtek.klusek2@gmail.com>
2024-05-13 23:28:29 +0200

    add nominatim status endpoint

--- src/api/other/router.py
+++ src/api/other/router.py
@@ -1,7 +1,9 @@
 import logging
+from typing import Dict
 
 from fastapi import APIRouter
 from src.api.other.schema import HealthzSchema
+from src.nominatim.services import NominatimService
 
 # Import router in src/__init__.py
 router = APIRouter(tags=['tools'])
@@ -11,3 +13,9 @@ logger = logging.getLogger()
 @router.get("/healthz", status_code=200, response_model=HealthzSchema)
 async def healthz():
     return {"status": "ok"}
+
+@router.get("/nominatim/status", status_code=200, response_model=Dict)
+async def nominatim_server_status():
+    service = NominatimService()
+    server_status = service.get_nominatim_status()
+    return server_status
